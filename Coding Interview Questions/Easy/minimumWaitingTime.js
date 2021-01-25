bc5e95382313ba53c16a4a71f180f933b1b344ba
Kathy Chan <kathykemopaw@gmail.com>
2021-01-24 21:04:52 -0800

    add minimumWaitingTime

---
+++ Coding Interview Questions/Easy/minimumWaitingTime.js
@@ -0,0 +1,12 @@
+function minimumWaitingTime(queries) {
+  // Write your code here.
+	queries.sort((a,b) => a -b);
+	let totalTime = 0;
+	
+	for(let i = 0; i < queries.length ; i ++){
+			let lastTime = queries[i];
+			let queriesLeft = queries.length - (i + 1); //lets you know how many times to multiply the "current" query by
+			totalTime += lastTime * queriesLeft; 
+	}
+	return totalTime;
+}
