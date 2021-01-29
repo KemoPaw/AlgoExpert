bd33544062acd43f615b0261f3aac3826725b925
Kathy Chan <kathykemopaw@gmail.com>
2021-01-29 15:21:36 -0800

    change spacing

--- CodeWars/VasyaClerk.js
+++ CodeWars/VasyaClerk.js
@@ -36,67 +36,69 @@ function tickets(peopleInLine){
   }
   
   return "YES";
+}
 
 
   // someone else's solution which I enjoy with the tuneries
 
-  //function Clerk(name) {
-//   this.name = name;
+  function Clerk(name) {
+  this.name = name;
   
-//   this.money = {
-//     25 : 0,
-//     50 : 0,
-//     100: 0 
-//   };
+  this.money = {
+    25 : 0,
+    50 : 0,
+    100: 0 
+  };
   
-//   this.sell = function(element, index, array) {
-//     this.money[element]++;
+  this.sell = function(element, index, array) {
+    this.money[element]++;
 
-//     switch (element) {
-//       case 25:
-//         return true;
-//       case 50:
-//         this.money[25]--;
-//         break;
-//       case 100:
-//         this.money[50] ? this.money[50]-- : this.money[25] -= 2;
-//         this.money[25]--;
-//         break;
-//     }
-//     return this.money[25] >= 0;
-//   };
-// }
+    switch (element) {
+      case 25:
+        return true;
+      case 50:
+        this.money[25]--;
+        break;
+      case 100:
+        this.money[50] ? this.money[50]-- : this.money[25] -= 2;
+        this.money[25]--;
+        break;
+    }
+    return this.money[25] >= 0;
+  };
+}
 
-// function tickets(peopleInLine){
-//   var vasya = new Clerk("Vasya");
-//   return peopleInLine.every(vasya.sell.bind(vasya)) ? "YES" : "NO";
-// }
-// }
+function tickets(peopleInLine){
+  var vasya = new Clerk("Vasya");
+  return peopleInLine.every(vasya.sell.bind(vasya)) ? "YES" : "NO";
+}
+
 
 // another solution: 
-// function tickets(peopleInLine) {
-//   var bills = [0, 0, 0]
-//   for (var i = 0; i < peopleInLine.length; i++) {
-//     switch (peopleInLine[i]) {
-//       case 25:
-//         bills[0]++
-//         break
+
+function tickets(peopleInLine) {
+  var bills = [0, 0, 0];
+    for (var i = 0; i < peopleInLine.length; i++) {
+        switch (peopleInLine[i]) {
+        case 25:
+            bills[0]++
+            break
+            
+        case 50:
+            bills[0]--
+            bills[1]++
+            break
+            
+        case 100:
+            bills[1] ? bills[1]-- : bills[0] -= 2
+            bills[0]--
+            break
+        }
         
-//       case 50:
-//         bills[0]--
-//         bills[1]++
-//         break
-        
-//       case 100:
-//         bills[1] ? bills[1]-- : bills[0] -= 2
-//         bills[0]--
-//         break
-//     }
-    
-//     if (bills[0] < 0) {
-//       return 'NO'
-//     }
-//   }
+        if (bills[0] < 0) {
+            return 'NO'
+        }
+    }
   
-//   return 'YES'
-// }
+  return 'YES'
+}
