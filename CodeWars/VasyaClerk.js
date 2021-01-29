e75b68ec878196ef95d9342c641c6092d51ee280
Kathy Chan <kathykemopaw@gmail.com>
2021-01-29 15:16:50 -0800

    Add solution

--- CodeWars/VasyaClerk.js
+++ CodeWars/VasyaClerk.js
@@ -72,3 +72,31 @@ function tickets(peopleInLine){
 //   return peopleInLine.every(vasya.sell.bind(vasya)) ? "YES" : "NO";
 // }
 // }
+
+// another solution: 
+// function tickets(peopleInLine) {
+//   var bills = [0, 0, 0]
+//   for (var i = 0; i < peopleInLine.length; i++) {
+//     switch (peopleInLine[i]) {
+//       case 25:
+//         bills[0]++
+//         break
+        
+//       case 50:
+//         bills[0]--
+//         bills[1]++
+//         break
+        
+//       case 100:
+//         bills[1] ? bills[1]-- : bills[0] -= 2
+//         bills[0]--
+//         break
+//     }
+    
+//     if (bills[0] < 0) {
+//       return 'NO'
+//     }
+//   }
+  
+//   return 'YES'
+// }
