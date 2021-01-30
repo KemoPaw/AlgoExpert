3a450453e85e692a041b8e1ff14657cb0569c5e4
Kathy Chan <kathykemopaw@gmail.com>
2021-01-29 16:50:50 -0800

    Add other solutions for IQ Test

--- CodeWars/iqTest.js
+++ CodeWars/iqTest.js
@@ -29,3 +29,32 @@ function iqTest(numbers){
   
   return Number(numStr);
 }
+
+//other person's solution 
+function iqTest(numbers){
+  numbers = numbers.split(' ')
+  
+  var evens = []
+  var odds = []
+  
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] & 1) {
+      odds.push(i + 1)
+    } else {
+      evens.push(i + 1)
+    }
+  }
+  
+  return evens.length === 1 ? evens[0] : odds[0]
+}
+
+//other person's solution 
+function iqTest(numbers){
+  numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+  
+  var odd = numbers.filter(function(el){ return el % 2 === 1});
+  var even = numbers.filter(function(el){ return el % 2 === 0});
+  
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+}
+
