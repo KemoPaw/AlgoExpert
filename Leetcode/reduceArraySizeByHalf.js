3f6331a11f3ddc2b1badf4e8219b1b0245a76bf2
Kathy Chan <kathykemopaw@gmail.com>
2021-02-05 23:00:07 -0800

    Edit syntax

--- Leetcode/reduceArraySizeByHalf.js
+++ Leetcode/reduceArraySizeByHalf.js
@@ -1,34 +1,34 @@
-Function leastCountArray(arr){ //[3,5,5,5,2,2,7,7,7,7]
+function leastCountArray(arr){ //[3,5,5,5,2,2,7,7,7,7]
 
-Let countObj = {3: 1, 5: 3, 2: 2, 7 : 4};
+let countObj = {3: 1, 5: 3, 2: 2, 7 : 4};
 
-For (let i = 0; i < arr.length; i ++){
+for (let i = 0; i < arr.length; i ++){
 	if(countObj[arr[i]] === undefined) {
 		countObj[arr[i]] = 1;
 } 
-Else {
+else {
 	countObj[arr[i]] += 1;
 }
 }
 
 
-Let countObjValues = Object.values(countObj).sort((a, b) => b - a);
+let countObjValues = Object.values(countObj).sort((a, b) => b - a);
 //[1, 3, 2, 4]; ===> [4, 3, 2, 1];
 
 //[1, 1, 1, 1, 1, 1]
-Let tempCount = 0;
-Let count = 0;
+let tempCount = 0;
+let count = 0;
 
-For (let j = 0; j < countObjValues.length; j ++){ //1
+for (let j = 0; j < countObjValues.length; j ++){ //1
 
 	tempCount += countObjValues[j]; // tempCount = 3;
-Count ++; // count = 3
+	count ++; // count = 3
 	
 	if(tempCount >= (arr.length / 2)){ // 3 >= 3;
-		Return count;
+		return count;
 }
 }
 
-Return count;
+return count;
 
 }
