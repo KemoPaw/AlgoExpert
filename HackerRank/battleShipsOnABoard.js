d8767490b863bf43e84f44fc93cd84170bcbfa1a
Kathy Chan <kathykemopaw@gmail.com>
2021-02-23 20:36:47 -0800

    Add Solution

---
+++ HackerRank/battleShipsOnABoard.js
@@ -0,0 +1,96 @@
+Function findNumShips(grid){
+	Let visited = []; //2d array of trues and falses;
+	Let counter = 0;
+
+	For (let i = 0; i < grid.length; i ++){
+		For (let k = 0; k < grid[i].length; k ++){
+			if(grid[i][k] === “.”){ //set as true; so I know I do not need to recheck it
+				Visited[i][k] = true;
+}
+			Else{
+Visited[i][k] = false;
+
+}}
+}
+
+//visited, if the ele is false, it needs to be counted at some point;
+
+	For (let m = 0; m < visited.length; m ++){
+		For (let q = 0; q < visited[m].length; q ++){
+
+			if(visited[m][q] === false){
+				//increase count
+				// know how long the ship is, and mark visited to true accordingly
+				
+				markVisitedShips([m, q], visited);
+				Counter += 1;
+				
+}
+
+
+
+}}
+}
+
+Function markVisitedShips(startPos, visited){ //startPos is an array [x, y]
+	Let dirs = [[0, 1],[1, 0],[0, -1],[-1, 0]];
+	
+	Let queue = [startPos]; // []
+	
+	while(queue.length !== 0){
+		Visited[queue[0]] = true;
+		Let newDirs = findDirs(queue[0], dirs, visited); //array of neighbors
+		queue.shift();
+		Queue = queue.concat(newDirs);
+}
+	
+	
+
+}
+
+Function findDirs(start, dirs, visited){
+	Let result = [];
+
+	For (let y = 0; y < dirs.length; y++){
+		Let currRow = start[0] + dirs[y][0];
+Let currCol = start[1] + dirs[y][1];
+
+If (currRow < 0 || currCol < 0 || currRow > visited.length || currCol > visited[0].length){
+	continue;
+}
+Else{
+	result.push([currRow, currCol]);
+}
+
+}
+
+Return result; //nested array;
+
+}
+
+	Return counter;
+}
+
+
+
+
+
+
+
+
+
+
+
+var countBattleships = function(board) {
+    let count = 0;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] === '.') continue;
+            if (board[i - 1] && board[i - 1][j] === "X") continue;
+            if (board[i][j - 1] === "X") continue;
+            count += 1;
+        }
+    }
+    return count
+};
+
