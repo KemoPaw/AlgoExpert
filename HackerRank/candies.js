391b9ec5ce82fc3dd70bbe8ce5088617b130e56c
Kathy Chan <kathykemopaw@gmail.com>
2021-03-03 23:58:02 -0800

    Add solution for candies

--- HackerRank/candies.js
+++ HackerRank/candies.js
@@ -1,38 +1,62 @@
 // https://www.hackerrank.com/challenges/candies/problem
 
-function candies(arr) {
-    let grades = arr;
-    let candies = new Array(grades.length);
-    //initialize to 0
-    candies.fill(0); //[0,0,0,0,1,0,0,0,0,1]
-    //construct a sorted array of student's grades
-    let unsortedGrades = [...grades];
-    grades.sort((a, b) => a - b); //[1, 1, 2, 2, 2, 4, 6, 7, 8, 9]
-    console.log(unsortedGrades);
-    console.log(grades);
-    for(let i = 0; i < grades.length; i++)//sortedGrades[i]: 1
-        {
-        let next_value_index = 0;
-        while((grades[i] !== unsortedGrades[next_value_index]) || (candies[next_value_index] !== 0))
-            {
-                next_value_index++;
-            }
-        //assign a value in candies according to values already in candies
-        let extra_candies = 0;
-        //is the current grade larger than the grades next to it? Act accordingly
-        if(unsortedGrades[next_value_index -1] && unsortedGrades[next_value_index] > unsortedGrades[next_value_index -1])//
-            {
-                extra_candies = candies[next_value_index-1];
-            }
-        if(unsortedGrades[next_value_index + 1] && unsortedGrades[next_value_index] > unsortedGrades[next_value_index + 1])//
-            {
-                extra_candies = Math.max(extra_candies, candies[next_value_index+1]);
-            }
-        candies[next_value_index] = extra_candies + 1;//
-        } 
-  console.log(candies)
-  return candies.reduce((a,b) => a+b, 0);
-//   return candies.sum();
-}
+// function candies(arr) {
+//     let grades = arr;
+//     let candies = new Array(grades.length);
+//     //initialize to 0
+//     candies.fill(0); //[0,0,0,0,1,0,0,0,0,1]
+//     //construct a sorted array of student's grades
+//     let unsortedGrades = [...grades];
+//     grades.sort((a, b) => a - b); //[1, 1, 2, 2, 2, 4, 6, 7, 8, 9]
+//     console.log(unsortedGrades);
+//     console.log(grades);
+//     for(let i = 0; i < grades.length; i++)//sortedGrades[i]: 1
+//         {
+//         let next_value_index = 0;
+//         while((grades[i] !== unsortedGrades[next_value_index]) || (candies[next_value_index] !== 0))
+//             {
+//                 next_value_index++;
+//             }
+//         //assign a value in candies according to values already in candies
+//         let extra_candies = 0;
+//         //is the current grade larger than the grades next to it? Act accordingly
+//         if(unsortedGrades[next_value_index -1] && unsortedGrades[next_value_index] > unsortedGrades[next_value_index -1])//
+//             {
+//                 extra_candies = candies[next_value_index-1];
+//             }
+//         if(unsortedGrades[next_value_index + 1] && unsortedGrades[next_value_index] > unsortedGrades[next_value_index + 1])//
+//             {
+//                 extra_candies = Math.max(extra_candies, candies[next_value_index+1]);
+//             }
+//         candies[next_value_index] = extra_candies + 1;//
+//         } 
+//   console.log(candies)
+//   return candies.reduce((a,b) => a+b, 0);
+// //   return candies.sum();
+// }
 
-candies(10, [2, 4, 2, 6, 1, 7, 8, 9, 2, 1]);
+// candies(10, [2, 4, 2, 6, 1, 7, 8, 9, 2, 1]);
+
+function countCandies(arr){
+    let candies = [1];
+    
+
+    for(let i = 1; i < arr.length; i ++){
+        if(arr[i] > arr[i - 1]){
+            candies.push(candies[candies.length] + 1);
+        }
+        else{
+            candies.push(1);
+        }
+        if(candies[i - 1] === candies[i] && arr[i] < arr[i - 1]){
+            candies[i - 1] ++;
+        }
+    }
+
+    let total = 0;
+
+    for(let j = 0; j < candies.length; j++){
+        total += candies[j];
+    }
+    return total;
+}
