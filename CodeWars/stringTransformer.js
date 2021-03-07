cbf825b7b6aad29f6beac4da48ebcdaa357b6214
Kathy Chan <kathykemopaw@gmail.com>
2021-03-06 21:29:53 -0800

    add solutions from codewars

---
+++ CodeWars/stringTransformer.js
@@ -0,0 +1,25 @@
+function stringTransformer(str) {
+  // Your code here
+  
+  let words = str.split(" ");
+  
+  for(let i = 0; i < words.length; i ++){
+    words[i] = changeCase(words[i]);
+  }
+  
+  return words.reverse().join(" ")
+}
+
+function changeCase(string){
+  let newStr = "";
+  
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === string[i].toUpperCase()){
+      newStr += string[i].toLowerCase();
+    }
+    else{
+      newStr += string[i].toUpperCase()
+    }
+  }
+  return newStr;
+}
