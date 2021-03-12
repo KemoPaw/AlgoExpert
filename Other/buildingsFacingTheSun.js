cf9e9c921ba8c4c55d2233e29f0cdd7e84311dd5
Kathy Chan <kathykemopaw@gmail.com>
2021-03-12 01:15:16 -0800

    Edit solution

--- Other/buildingsFacingTheSun.js
+++ Other/buildingsFacingTheSun.js
@@ -1,39 +1,38 @@
-Function canSeeSun(buildingHeights, dir){
+function canSeeSun(buildingHeights, dir){
 
-if(buildingHeights.length === 0) return [];
-if(buildingHeights.length === 1) return [0];
+	if(buildingHeights.length === 0) return [];
+	if(buildingHeights.length === 1) return [0];
 
-Let currHighestBuilding = 0; //5
-Let resultIndices = []; // [7, 6, 3, 1]
+	let currHighestBuilding = 0; //5
+	let resultIndices = []; // [7, 6, 3, 1]
 
 	if(dir === “EAST”){ //right to left;
 
 		currHighestBuilding = buildingHeights[buildingHeights.length - 1];
 		resultIndices.push(buildingHeights.length - 1);
 
-		For (let i = buildingHeights.length - 2; i >= 0; i --){ // i = 0;
-			Let currHeight = buildingHeights[i]; //3
-			If (currHeight > currHighestBuilding){ //  3 > 5
+		for (let i = buildingHeights.length - 2; i >= 0; i --){ // i = 0;
+			let currHeight = buildingHeights[i]; //3
+			if (currHeight > currHighestBuilding){ //  3 > 5
 				currHighestBuilding = currHeight;
 				resultIndices.push(i);
-}
-}
-}
+			}
+		}
+	}
 
-if(dir === “WEST”){ //left to right;
-	currHighestBuilding = buildingHeights[0];
+	if(dir === “WEST”){ //left to right;
+		currHighestBuilding = buildingHeights[0];
 		resultIndices.push(0);
-
-		For (let i = 1; i < buildingHeights.length; i ++){
-			Let currHeight = buildingHeights[i];
-			If (currHeight > currHighestBuilding){
+		for (let i = 1; i < buildingHeights.length; i ++){
+			let currHeight = buildingHeights[i];
+			if (currHeight > currHighestBuilding){
 				currHighestBuilding = currHeight;
 				resultIndices.push(i);
-}
-}
-}
+			}
+		}
+	}
 
-Return resultIndices.sort((a, b)=> a - b);
+return resultIndices.sort((a, b)=> a - b);
 
 }
 
